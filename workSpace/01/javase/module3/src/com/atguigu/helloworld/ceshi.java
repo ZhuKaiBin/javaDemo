e9403d6a064bbbf1960caefdd289e58c953b211c
zhukb <912549963@qq.com>
2024-07-08 21:34:27 +0800

    修改

--- workSpace/01/javase/module3/src/com/atguigu/helloworld/ceshi.java
+++ workSpace/01/javase/module3/src/com/atguigu/helloworld/ceshi.java
@@ -4,10 +4,8 @@ public class ceshi {
     public static void main(String[] args) {
         double a=2.5;
         System.out.println("新增");
-
-
-
-
-
+        /**
+        master1fg
+         */
     }
 }
